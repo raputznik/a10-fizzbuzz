e2ef3d0db7528e1d5799df6bbae7fbd6af3d6ee7
Jeremy Dahl <jeremy@palebluedot.ca>
2013-07-09 15:31:08 -0700

    Moved if statements inside the "for" operator
    I figured out that the if statements could not be outside the for
    statement. Also, needed to execute the commands in the right order.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,18 +1,27 @@
 
 <?php
 
-// $n = 1;
-// while ($n <= 10) {
-//     echo "The number is " . $n++ . ".<br>"; 
-
-// }
-
 for ($n = 1; $n <= 10; $n++) {
-	echo "The number is " . $n . ".<br>";
+
+	if ($n % 3 == 0 && $n % 2 == 0) {
+	echo "The number is " . $n . " fizz buzz.<br>" ;
+	}
+
+
+	else if ($n % 2 == 0) {
+		echo "The number is " . $n . " fizz.<br>" ;
+	}
+
+	else if ($n % 3 == 0) {
+		echo "The number is " . $n . " buzz.<br>" ;
+	}
+
+
+	else {
+			echo "The number is " . $n . ".<br>";
+	}
+
 }
 
-if ($n % 2 == 0) {
-	echo " fizz";
-}
 
 ?>
