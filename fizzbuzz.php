50c1440773256f4323caa5aacae8be4af14e829e
Jeremy Dahl <jeremy@palebluedot.ca>
2013-07-08 19:52:50 -0700

    Initial Sync
    Got the list working, need to figure out the "fizz" and the "buzz"...

---
+++ fizzbuzz.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// $n = 1;
+// while ($n <= 10) {
+//     echo "The number is " . $n++ . ".<br>"; 
+
+// }
+
+for ($n = 1; $n <= 10; $n++) {
+	echo "The number is " . $n . ".<br>";
+}
+
+if ($n % 2 == 0) {
+	echo " fizz";
+}
+
+?>
